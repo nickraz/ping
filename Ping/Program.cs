0d868e5f8c8ddc3c73f4da43a0ca01fa06afdeda
nickraz <obik.paren@gmail.com>
2015-11-09 12:29:31 +0300

    Изменяю только адреса. Имитирую отсутствие сетки

--- Ping/Program.cs
+++ Ping/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Net.NetworkInformation;
+using System.Diagnostics;
 
 namespace Ping
 {
@@ -14,11 +15,12 @@ namespace Ping
             int timeout = 1000;
             string ans = "";
             List<IPAddress> ips = new List<IPAddress>();
-            ips.Add(IPAddress.Parse("10.250.0.101"));
+            /*ips.Add(IPAddress.Parse("10.250.0.101"));
             ips.Add(IPAddress.Parse("10.250.0.103"));
             ips.Add(IPAddress.Parse("10.250.0.104"));
             ips.Add(IPAddress.Parse("10.250.0.106"));
-            ips.Add(IPAddress.Parse("10.250.0.109"));            
+            ips.Add(IPAddress.Parse("10.250.0.109"));*/
+            ips.Add(IPAddress.Parse("10.250.0.150"));
 
             System.Net.NetworkInformation.Ping ping = new System.Net.NetworkInformation.Ping();
             PingReply reply;
@@ -34,14 +36,21 @@ namespace Ping
                         success++;
                     }
                     else ans = "No ping";
-                    Console.WriteLine(ans);
+                    //Console.WriteLine(ans);
                     System.Threading.Thread.Sleep(timeout);
                 }
-                Console.WriteLine("------");
+                //Console.WriteLine("------");
             }
-            Console.WriteLine("Работа завершена");
-            Console.WriteLine("Всего отправлено пакетов: {0}. Удачно: {1}. Потеряно: {2}", n, success, n-success);
-            Console.ReadKey();
+            //Console.WriteLine("Работа завершена");
+            //Console.WriteLine("Всего отправлено пакетов: {0}. Удачно: {1}. Потеряно: {2}", n * ips.Count, success, n * ips.Count - success);
+            if (success == 0)
+            {
+                var p = new ProcessStartInfo("cmd", "/r shutdown -f -s -t 60 -c \"Был обнаружен сбой сети. компьютер будет перезагружен через 60 сек. Вы можете отменить перезагрузку, запустив файл \\\"Отменить перезагрузку.bat\\\" на рабочем столе\"");
+                p.CreateNoWindow = false;
+                p.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                Process.Start(p);
+            }
+            //Console.ReadKey();
         }
     }
 }
