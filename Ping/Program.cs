ea2a0d8b5e97118c8a1fc61d064a060ccbd4b274
nickraz <obik.paren@gmail.com>
2015-11-09 10:50:54 +0300

    Пинг заработал

--- Ping/Program.cs
+++ Ping/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net.NetworkInformation;
+using System.Net;
 
 namespace Ping
 {
@@ -9,6 +11,19 @@ namespace Ping
     {
         static void Main(string[] args)
         {
+            int n = 4;
+            int timeout = 1000;
+            string ans = "";
+            System.Net.NetworkInformation.Ping ping = new System.Net.NetworkInformation.Ping();
+            PingReply reply;
+            for (int i = 0; i < n; i++)
+            {
+                reply = ping.Send(IPAddress.Parse("10.250.0.102"), timeout);
+                ans = String.Format("Адрес: {0}, TTL: {1}, Статус: {2}", reply.Address, reply.Options.Ttl, reply.Status);
+                Console.WriteLine(ans);
+                System.Threading.Thread.Sleep(timeout);
+            }
+            Console.ReadKey();
         }
     }
 }
